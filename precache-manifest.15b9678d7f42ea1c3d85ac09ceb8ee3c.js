b783510dfb808e399f2eabdf4d3483ee73a6c31a
RBianchi-Al <renatalebianchi@gmail.com>
2020-04-17 13:28:42 -0300

    Updates

---
+++ precache-manifest.15b9678d7f42ea1c3d85ac09ceb8ee3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086b4b568ede17aee85395b3eb95cd89",
+    "url": "/repository/index.html"
+  },
+  {
+    "revision": "40af0c0bc1737140eaf6",
+    "url": "/repository/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d7ad54ddacb5444c1ded",
+    "url": "/repository/static/js/2.cc6ea93c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repository/static/js/2.cc6ea93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40af0c0bc1737140eaf6",
+    "url": "/repository/static/js/main.391c94a1.chunk.js"
+  },
+  {
+    "revision": "a6f1f48401a381beb31d",
+    "url": "/repository/static/js/runtime-main.1de469bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/repository/static/media/logo.5d5d9eef.svg"
+  }
+]);
